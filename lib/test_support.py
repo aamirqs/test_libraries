1cf7c588aa6f80d0d0212d19f2efb07211596e9a
Aamir Shaikh <aamir@portworx.com>
2019-12-30 16:53:37 +0530

    add sudoku solver script

--- lib/test_support.py
+++ lib/test_support.py
@@ -1,12 +1,22 @@
 class TestSupport:
-    def test_assert(self, flag):
+    def test_assert(self, expression, message):
+        if not expression:
+            raise Exception("")
+
         pass
 
-    def test_assert_expected(self):
+    def test_assert_expected(self, actual, expected, message):
+        return self.test_assert(str(actual).strip() == str(expected).strip(), message)
+
+    def test_critical(self, message):
         pass
 
     def logger(self):
         pass
+
+
+class Error(Exception):
+    pass
 
 
 test_obj = TestSupport()
